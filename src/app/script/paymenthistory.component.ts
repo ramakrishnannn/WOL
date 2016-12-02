86308ba5d08bb26729f76cb2258c1356386c1c01
ramakrishnan.g <ramakrishnan.g@prodapt.com>
2016-12-02 19:36:15 +0530

    payment files update by kalanithi

--- src/app/script/paymenthistory.component.ts
+++ src/app/script/paymenthistory.component.ts
@@ -3,8 +3,8 @@ import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-paymenthistory',
-  templateUrl: '../html/paymenthistory.component.html',
-  styleUrls: ['../css/paymenthistory.component.css']
+  templateUrl: './paymenthistory.component.html',
+  styleUrls: ['./paymenthistory.component.css']
 })
 export class PaymenthistoryComponent implements OnInit {
 
@@ -14,17 +14,44 @@ export class PaymenthistoryComponent implements OnInit {
     public sortBy = "paidDate";
     public sortOrder = "desc";
 
+  public paymentTableData = [
+  {
+    "mode": "Credit Card Payments",
+    "paidDate": "04/04/2016",
+    "paidAmount": 100.00
+  },
+  {
+    "mode": "NEFT",
+    "paidDate": "02/01/2016",
+    "paidAmount": 80
+  },
+  {
+    "mode": "ACH Payments",
+    "paidDate": "03/07/2016",
+    "paidAmount": 100
+  },
+  {
+    "mode": "Credit Card Payments",
+    "paidDate": "06/06/2016",
+    "paidAmount": 50
+  },
+  {
+    "mode": "ACH Payments",
+    "paidDate": "07/04/2016",
+    "paidAmount": 100
+  },
+  {
+    "mode": "Credit Card Payments",
+    "paidDate": "08/01/2016",
+    "paidAmount": 50
+  }
+];
+
     constructor(private http: Http) {
     }
 
     ngOnInit(): void {
-       /* this.http.get("./payment_data.json")
-            .subscribe((data)=> {
-                setTimeout(()=> {
-                    this.data = data.json();
-                }, 1000);
-            });*/
-            
+        this.data = this.paymentTableData;
     }
 
     public toInt(num: string) {
@@ -40,4 +67,10 @@ export class PaymenthistoryComponent implements OnInit {
     }
   
     
+}
+
+
+export class SmartTablesService {
+
+ 
 }
