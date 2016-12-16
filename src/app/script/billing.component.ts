6403c76216c8a0a8dd6a469ed9fb95e5594493e2
kalanidhis <skalanidhi22@gmail.com>
2016-12-16 19:48:10 +0530

    Add files via upload

--- src/app/script/billing.component.ts
+++ src/app/script/billing.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-billing',
   templateUrl: '../html/billing.component.html',
   styleUrls: ['../css/billing.component.css']
 })
-export class BillingComponent implements OnInit {
+export class BillingComponent {
 
+ dateval: string;
+public invoiceData;
 
+  constructor(private http: Http) {
+    }
+     
+	getBillingDetails(monthVal){
 
-  constructor() { }
+	if (monthVal.length > 5) {
 
-  ngOnInit() {
-  }
+		var res = monthVal.split("-");
+		var obj = { 
+            monthName: res[0],
+            monthId: new Date(monthVal+'-1-01').getMonth()+1+"",
+            year: res[1]+""
+        }; 
+	    var jsonString= JSON.stringify(obj);
 
+      this.http.post('http://localhost:8080/invoice/billingdetails',jsonString)
+      .subscribe(
+        data => { this.invoiceData = data.json()},
+        err => console.error(err),
+        () => console.log(this.invoiceData)
+      );
+	}
+
+   }
 }
