467efa79fcec147d1147dd1c65fab9aed6939af5
ramakrishnan.g <ramakrishnan.g@prodapt.com>
2016-12-27 18:03:47 +0530

    new design update part1

---
+++ src/app/loading/loading.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'loading',
+    templateUrl: 'loading.html',
+    styleUrls: ['loading.css']
+
+})
+export class LoadingComponent{
+
+}
